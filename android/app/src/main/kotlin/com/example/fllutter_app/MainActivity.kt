95dcc816f3a2632c083c60dc7063ed0acf997480
shunkominato <shun19871001@gmail.com>
2022-07-01 22:51:19 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/fllutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fllutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/time_line/time_line_page.dart
@@ -0,0 +1,104 @@
+import 'package:fllutter_app/view/time_line/post_page.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../model/account.dart';
+import '../../model/post.dart';
+
+class TimeLinePage extends StatefulWidget {
+  const TimeLinePage({Key? key}) : super(key: key);
+
+  @override
+  State<TimeLinePage> createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  Account myAccount = Account(
+    id: '1',
+    name: 'test',
+    selfIntroduction: 'intro',
+    userId: 'aaaa',
+    imagePath: 'https://images.app.goo.gl/TV2aTDJDjdh9sxJd9',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now(),
+  );
+
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: 'hello',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: 'helloppppppp',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('timeline', style: TextStyle(color: Colors.black),),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 2,
+      ),
+      body: ListView.builder(
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: index == 0 ? Border(
+                top: BorderSide(color: Colors.grey, width: 0),
+                bottom: BorderSide(color: Colors.grey, width: 0),
+              ) : Border(bottom: BorderSide(color: Colors.grey, width: 0)
+              )
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  foregroundImage: NetworkImage(myAccount.imagePath),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                                Text('@${myAccount.userId}', style: TextStyle(color: Colors.grey),),
+                              ],
+                            ),
+
+                            Text(DateFormat('M/d/yy').format(postList[index].createdTime!))
+                          ],
+                        ),
+                        Text(postList[index].content)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage()));
+        },
+        child: Icon(Icons.chat_bubble_outline),
+      ),
+    );
+  }
+}
