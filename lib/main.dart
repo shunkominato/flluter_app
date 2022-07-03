accdef683e037b09218ebd3ceff577c117b10b70
shunkominato <shun19871001@gmail.com>
2022-07-03 10:29:59 +0900

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fllutter_app/view/screen.dart';
+import 'package:fllutter_app/view/state_up/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Screen(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/view/state_up/login_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../screen.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPateState();
+}
+
+class _LoginPateState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text('sns', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: 'メールアドレス'
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: passController,
+                  decoration: InputDecoration(
+                      hintText: 'パスワード'
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              RichText(text: TextSpan(
+                style: TextStyle(color: Colors.black),
+                children: [
+                  TextSpan(text: 'アカウントを作成していない方は'),
+                  TextSpan(text: 'こちら',
+                    style: TextStyle(color: Colors.blue),
+                    recognizer: TapGestureRecognizer()..onTap = () {
+                      print('jkjddk');
+                    }
+                  )
+                ]
+              )),
+              SizedBox(height: 70),
+              ElevatedButton(onPressed: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Screen()));
+              }, child: Text('emailでログイン'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
